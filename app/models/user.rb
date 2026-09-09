410c452b60f37a6d798ed29e6095609b3147368c
CRJL1001 <constantlescutprive2@gmail.com>
2026-09-09 17:39:54 +0200

    user_id -> articles relation OK

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :articles
   validates :username, presence: true, length: { minimum: 3, maximum: 25 }, uniqueness: { case_sensitive: false } 
   VALIDE_EMAIL_REGEX = /\A[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\z/
   validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false }, format: { with: VALIDE_EMAIL_REGEX }
