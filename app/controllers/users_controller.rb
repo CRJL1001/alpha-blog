aafcbc76615017fdd185f2e6ef19fb873eb587ca
CRJL1001 <constantlescutprive2@gmail.com>
2026-09-10 11:52:35 +0200

    Edit account OK

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,22 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Compte modifié avec succès"
+      redirect_to articles_path
+    else
+      render :edit, status: :unprocessable_content
+    end     
+  end
+
+
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -15,12 +31,14 @@ class UsersController < ApplicationController
 
   end
 
+  
+
+
   private 
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-
   
 
 end
