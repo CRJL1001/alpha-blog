c976b2a886054fd4fbb24aed4bebcf66674a6c5e
CRJL1001 <constantlescutprive2@gmail.com>
2026-09-11 11:42:36 +0200

    Test OK et categories show, index, new, ok

---
+++ test/integration/create_category_test.rb
@@ -0,0 +1,36 @@
+require "test_helper"
+
+class CreateCategoryTest < ActionDispatch::IntegrationTest
+  test "get new category form and create category" do
+    #test d'accès au formulaire
+    get "/categories/new" #accède à la page new
+    assert_response :success #vérifie que la réponse est OK
+
+    #test de création
+    assert_difference 'Category.count', 1 do # Vas vérifié que après la boucle le nbr de catégorie sera augmenter de 1
+      post categories_path, params: {category: { name: "Sports" }} #requête de création d'une catégorie
+
+      #test de redirection
+      assert_response :redirect #vérifie que la réponse de redirection est OK
+    end
+    follow_redirect! #suit la redirection
+    assert_response :success #réponse de la page de redirection OK
+    assert_match "Sports", response.body #vérifie que le nom de la category est présent quelque part dans le html.body
+  end
+
+  test "get new category form and rejecte invalid category submission" do
+    #test d'accès au formulaire
+    get "/categories/new" #accède à la page new
+    assert_response :success #vérifie que la réponse est OK
+
+    #test de création
+    assert_no_difference 'Category.count' do # Vas vérifié que après la boucle le nbr de catégorie ne changera pas
+      post categories_path, params: {category: { name: "" }} #requête de création d'une catégorie au nom vide
+    end
+    #test affichage erreurs
+    assert_match "impossible(s)", response.body #présence de impossible(s) dans le body
+    assert_select "div.alert" #présence d'une div avec la classe alert
+    assert_select "h4.alert-heading" #présence d'un h4 avec la classe alert-heading
+  end
+
+end
